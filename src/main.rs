54de3ee87888b20f34f283f1542e2a03bc5fec7a
Nurettin Berkay Demirkol <90573081+nurettinberkaydemirkol@users.noreply.github.com>
2025-03-01 18:59:41 +0300

    add custom logger for boot screen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -10,15 +11,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    let hello = b"Hello World!";
-    
-    for (i, &byte) in hello.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset((i * 2) as isize) = byte;
-            *vga_buffer.offset((i * 2 + 1) as isize) = 0x0f; 
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
